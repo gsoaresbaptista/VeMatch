1099e897fbc6c26760f052443c7b1128bd041eab
Gabriel Soares baptista <gsoaresbaptista@gmail.com>
2024-12-20 10:52:42 -0300

    🎉 begin: add initial code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# VeMatch
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://github.com/gsoaresbaptista/VeMatch/raw/main/assets/logo/white.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://github.com/gsoaresbaptista/VeMatch/raw/main/assets/logo/black.svg">
+  <img alt="SVG Image" width="75px" height="75px" align="left">
+</picture>
+
+# VeMatch: Image Similarity Toolkit
+
+## About
+VeMatch is a Python library designed to implement state-of-the-art techniques for image similarity identification. It is ideal for researchers and developers working on re-identification (ReID) and embedding analysis tasks. VeMatch provides practical and customizable tools for manipulating datasets and similarity algorithms.
+
+
+## Installation
+Install the package via pip:
+```bash
+pip install VeMatch
+```
+
+## Roadmap
+TO-DO
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VeMatch",
+    version="0.0.1",
+    packages=find_packages(),
+    package_data={"VeMatch": ["datasets/datasets.yaml"]},
+    install_requires=["requests", "PyYAML", "tqdm", "torch"],
+    python_requires=">=3.10",
+    description="A comprehensive toolkit for managing datasets and implementing advanced techniques for similar image identification.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Gabriel Soares Baptista",
+    author_email="gsoaresbaptista@gmail.com",
+    url="https://github.com/gsoaresbaptista/VeMatch",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
